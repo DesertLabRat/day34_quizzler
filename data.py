af1c90d102fcec2f75b7467126e0dab7eb7cb6c6
DesertBorn95 <delaney.seeburger@gmail.com>
2026-08-11 14:10:34 -0700

    initial commit

---
+++ data.py
@@ -0,0 +1,13 @@
+import requests
+
+PARAMETERS = {
+    "amount": 10,
+    "type": "boolean",
+}
+
+response = requests.get(url=f"https://opentdb.com/api.php?", params=PARAMETERS)
+response.raise_for_status()
+
+data = response.json()
+question_data = data["results"]
+print(question_data)
