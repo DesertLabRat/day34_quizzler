fc0b05346ee67184ae21783794cf10af8252fc48
DesertBorn95 <delaney.seeburger@gmail.com>
2026-08-11 14:11:02 -0700

    user interface code

---
+++ ui.py
@@ -0,0 +1,29 @@
+THEME_COLOR = "#375362"
+
+import tkinter as tk
+
+class Quiz_Interface():
+
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = tk.Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = tk.Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(150, 125, text="some text", fill=THEME_COLOR, font=("Arial", 20, "italic"))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_image = tk.PhotoImage(file="images/true.png")
+        self.true_button = tk.Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = tk.PhotoImage(file="images/false.png")
+        self.false_button = tk.Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
+
+
+        self.window.mainloop()
+
